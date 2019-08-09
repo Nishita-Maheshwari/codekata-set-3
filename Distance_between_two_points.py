2673d8e18dfe957f394d5378e97d584e37c42a3d
Nishita-Maheshwari <nishita.maheshwari55@gmail.com>
2019-08-09 16:59:29 +0530

    Create Distance_between_two_points.py
    # we can input in one line
    #x1, x2, x3, x4 = eval(input("enter the value x1,x2,x3,y1,y2') 
    # Formula  
    #distance =((x1-x2)**2+(y2-y1)**2)**0.5

---
+++ Distance_between_two_points.py
@@ -0,0 +1,8 @@
+x1 = int(input("first coordinates"))
+x2 = int(input("second coordinates"))
+y1 = int(input("third coordinates"))
+y2 = int(input("fourth coordinates"))
+
+d=((x1-x2)**2+(y2-y1)**2)**0.5
+print("distance",d)
+
