f5e0be7618ce7c48df4d2b20c27dd5350201368d
Nishita-Maheshwari <nishita.maheshwari55@gmail.com>
2020-02-16 00:22:19 +0530

    Update Reverse words.py

--- Reverse words.py
+++ Reverse words.py
@@ -7,8 +7,8 @@
 
 t =int(input())
 while(t!=0):
-    string=input()
-    list_string=string.split(".")
-    list_string.reverse()
-    print(".".join(list_string))
+    s=input()
+    ls=s.split(".")
+    ls.reverse()
+    print(".".join(ls))
     t-=1
