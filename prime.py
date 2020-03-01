49d61f33e5d77dfeda6d7430b07e33f65033434c
Nishita-Maheshwari <nishita.maheshwari55@gmail.com>
2020-03-01 15:39:11 +0530

    init commit

---
+++ prime.py
@@ -0,0 +1,12 @@
+def prime(n):
+    for i in range(2,n):
+        if(n%i)==0:
+            print(n,"is not aa prime number")
+            break
+    else:
+        print(n,"is a prime number")
+num = int(input())
+if(num>0):
+    prime(num)
+else:
+    print("enter a positive number ")
